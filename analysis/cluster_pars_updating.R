3e74a3ce307701c3a453ffe3d1d5012c2105d7a7
Oliver Watson <ow813@dide.local>
2021-12-03 10:43:49 +0000

    Final results

--- analysis/cluster_pars_updating.R
+++ analysis/cluster_pars_updating.R
@@ -26,16 +26,16 @@ get_best <- function(out) {
       best$scaling_factor <- tail(na.omit(out$pmcmc_results$scaling_factor),1)
     }
 
-    # best$covariance_matrix[[1]][,] <- 0.3
-    # best$scaling_factor <- 1
+    best$covariance_matrix[[1]][,] <- 0.03
+    best$scaling_factor <- 1
     best$province <- out$parameters$province
 
     return(best)
   }
 }
 
-# central pars
-pars_list <- lapply(grp$X[grp$status()=="COMPLETE"], function(x) {
+# best pars
+pars_list_best <- lapply(grp_best$X[grp_best$status()=="COMPLETE"], function(x) {
   out <- readRDS(
     gzcon(
       unz(
@@ -47,9 +47,21 @@ pars_list <- lapply(grp$X[grp$status()=="COMPLETE"], function(x) {
   get_best(out)
 })
 
-# vacc one from earlier that we need
-grp_vacc <- obj$task_bundle_get("orderly_bundles_derived_vaccine_initial")
-pars_list2 <- lapply(grp_vacc$X[23], function(x) {
+# central pars
+pars_list_central <- lapply(grp_central$X[grp_central$status()=="COMPLETE"], function(x) {
+  out <- readRDS(
+    gzcon(
+      unz(
+        gsub("raw", "derived", x),
+        file.path(gsub("\\.zip", "", basename(x)), "pack/res.rds")
+      )
+    )
+  )
+  get_best(out)
+})
+
+# worst pars
+pars_list_worst <- lapply(grp_worst$X[grp_worst$status()=="COMPLETE"], function(x) {
   out <- readRDS(
     gzcon(
       unz(
@@ -64,18 +76,234 @@ pars_list2 <- lapply(grp_vacc$X[23], function(x) {
 # get the old conditions
 pars_init <- readRDS(file.path(here::here(), "src/prov_fit/pars_init.rds"))
 
-names(pars_list) <- unlist(lapply(pars_list, "[[", "province"))
-names(pars_list2) <- unlist(lapply(pars_list2, "[[", "province"))
-pars_init$central[match(names(pars_list), names(pars_init$central))] <- pars_list
-pars_init$central[match(names(pars_list2), names(pars_init$central))] <- pars_list2
+names(pars_list_best) <- unlist(lapply(pars_list_best, "[[", "province"))
+names(pars_list_central) <- unlist(lapply(pars_list_central, "[[", "province"))
+names(pars_list_worst) <- unlist(lapply(pars_list_worst, "[[", "province"))
 
-pars_init$optimistic <- pars_init$central
-pars_init$worst <- pars_init$central
+pars_init$optimistic[match(names(pars_list_best), names(pars_init$optimistic))] <- pars_list_best
+pars_init$central[match(names(pars_list_central), names(pars_init$central))] <- pars_list_central
+pars_init$worst[match(names(pars_list_worst), names(pars_init$worst))] <- pars_list_worst
 
 # save to location
 saveRDS(pars_init, file.path(here::here(), "src/prov_fit/pars_init.rds"))
 
 
-## attempt at smoothing
+# Manual tweaking
 
-pars_init$central$Alborz$R0
+source("src/prov_fit/R/spline_fit.R")
+source("src/prov_fit/R/vaccine.R")
+source("src/prov_fit/R/plotting.R")
+
+plot_pars_deaths <- function(res, pars) {
+
+  ll <- iran_log_likelihood(pars = pars,
+                            data = res$pmcmc_results$inputs$data,
+                            squire_model = res$pmcmc_results$inputs$squire_model,
+                            model_params = res$pmcmc_results$inputs$model_params,
+                            pars_obs = res$pmcmc_results$inputs$pars_obs,
+                            n_particles = 2, forecast_days = 0, return = "full",
+                            Rt_args = res$pmcmc_results$inputs$Rt_args,
+                            interventions = res$pmcmc_results$inputs$interventions)
+
+  index <- nimue:::odin_index(res$model)
+  deaths <- diff(rowSums(ll[,index$D]))
+  df <- data.frame("deaths" = deaths, date = as.Date(names(deaths)))
+  ggplot(df, aes(date, deaths)) + geom_line() +
+    geom_point(aes(date, deaths/7), res$pmcmc_results$inputs$data) +
+    geom_smooth(aes(date, deaths/7), res$pmcmc_results$inputs$data, span = 0.1)
+
+}
+
+get_ll <- function(res, pars) {
+
+  ll <- iran_log_likelihood(pars = pars,
+                            data = res$pmcmc_results$inputs$data,
+                            squire_model = res$pmcmc_results$inputs$squire_model,
+                            model_params = res$pmcmc_results$inputs$model_params,
+                            pars_obs = res$pmcmc_results$inputs$pars_obs,
+                            n_particles = 2, forecast_days = 0, return = "ll",
+                            Rt_args = res$pmcmc_results$inputs$Rt_args,
+                            interventions = res$pmcmc_results$inputs$interventions)$log_likelihood
+  return(ll)
+
+}
+
+fix_ll <- function(res, pars, min = 0.5, max = 2, n_span = 8) {
+
+  # get the full data we are fitting to
+  data_full <- res$pmcmc_results$inputs$data
+  days <- 0
+
+  # this was eventually going to be used to check which weeks are affected by which Rt changes
+  # but mine are all pegged to a start date the same number of days before the first week of deaths
+  # so this isn't needed
+  days_impacted_by_rt <- lapply(seq_len(length(grep("Rt_rw", names(pars)))), function(x) {
+    (1 + 2 + ((x - 1) * 14)) : (1 + 2 + ((x) * 14)) + 20
+  })
+
+  # loop over 2 week periods and scan over changing the relevant Rt_rw and updating in pars
+  for(i in seq(2, nrow(data_full),2)) {
+
+    pars_i <- pars
+    data_i <- data_full[seq_len(i),]
+    days <- seq(max(days),  max(days) + as.integer(max(data_i$date) - max(pars$start_date)), 1)
+
+    ll <- iran_log_likelihood(pars = pars,
+                              data = data_i,
+                              squire_model = res$pmcmc_results$inputs$squire_model,
+                              model_params = res$pmcmc_results$inputs$model_params,
+                              pars_obs = res$pmcmc_results$inputs$pars_obs,
+                              n_particles = 2, forecast_days = 0, return = "ll",
+                              Rt_args = res$pmcmc_results$inputs$Rt_args,
+                              interventions = res$pmcmc_results$inputs$interventions)$log_likelihood
+
+    poss <- seq(pars[[paste0("Rt_rw_", i/2)]] - min, pars[[paste0("Rt_rw_", i/2)]] + max, length.out = n_span)
+    poss <- c(poss, pars[[paste0("Rt_rw_", i/2)]])
+    ll_i <- c(rep(0, n_span), ll)
+
+    for(j in seq_len(n_span)){
+      pars_i[[paste0("Rt_rw_", i/2)]] <- poss[j]
+      ll_i[j] <- iran_log_likelihood(pars = pars_i,
+                                     data = data_i,
+                                     squire_model = res$pmcmc_results$inputs$squire_model,
+                                     model_params = res$pmcmc_results$inputs$model_params,
+                                     pars_obs = res$pmcmc_results$inputs$pars_obs,
+                                     n_particles = 2, forecast_days = 0, return = "ll",
+                                     Rt_args = res$pmcmc_results$inputs$Rt_args,
+                                     interventions = res$pmcmc_results$inputs$interventions)$log_likelihood
+    }
+    pars[[paste0("Rt_rw_", i/2)]] <- poss[which.max(ll_i)]
+  }
+
+  return(pars)
+
+}
+
+# --------------------------
+## tehran best
+# --------------------------
+
+res <- fetch_res(grp_best, 8)
+pars <- res$pmcmc_results$results[20001,]
+pars$start_date <- squire:::offset_to_start_date(res$pmcmc_results$inputs$data$date[1],start_date = pars$start_date)
+
+plot_pars_deaths(res, pars)
+pars_new <- fix_ll(res, pars, 0.95, 1.15, 4)
+plot_pars_deaths(res, pars_new)
+
+pars_init$optimistic$Tehran[seq_len(length(pars)-3)] <- head(as.numeric(pars_new[1,]), -3)
+pars_init$optimistic$Tehran$start_date <- lubridate::as_date(pars_init$optimistic$Tehran$start_date)
+
+# --------------------------
+## EA central
+# --------------------------
+
+res <- fetch_res(grp_central, 1)
+pars <- res$pmcmc_results$results[20001,]
+pars$start_date <- squire:::offset_to_start_date(res$pmcmc_results$inputs$data$date[1],start_date = pars$start_date)
+
+plot_pars_deaths(res, pars)
+pars_new <- fix_ll(res, pars)
+plot_pars_deaths(res, pars_new)
+
+pars_init$central$`East Azerbaijan`[seq_len(length(pars)-3)] <- head(as.numeric(pars_new3[1,]), -3)
+pars_init$central$`East Azerbaijan`$start_date <- lubridate::as_date(pars_init$central$`East Azerbaijan`$start_date)
+
+# --------------------------
+## RK central
+# --------------------------
+
+res <- fetch_res(grp_central, 11)
+pars <- res$pmcmc_results$results[which.max(res$pmcmc_results$results$log_posterior),]
+pars$start_date <- squire:::offset_to_start_date(res$pmcmc_results$inputs$data$date[1],start_date = pars$start_date)
+
+plot_pars_deaths(res, pars)
+pars_new <- fix_ll(res, pars)
+plot_pars_deaths(res, pars_new)
+
+pars_init$central$`Razavi Khorasan`[seq_len(length(pars)-3)] <- head(as.numeric(pars_new[1,]), -3)
+pars_init$central$`Razavi Khorasan`$start_date <- lubridate::as_date(pars_init$central$`Razavi Khorasan`$start_date)
+
+# --------------------------
+## Tehran central
+# --------------------------
+
+res <- fetch_res(grp_central, 8)
+pars <- res$pmcmc_results$results[which.max(res$pmcmc_results$results$log_posterior),]
+pars$start_date <- squire:::offset_to_start_date(res$pmcmc_results$inputs$data$date[1],start_date = pars$start_date)
+
+plot_pars_deaths(res, pars)
+pars_new <- fix_ll(res, pars)
+plot_pars_deaths(res, pars_new)
+
+pars_init$central$Tehran[seq_len(length(pars)-3)] <- head(as.numeric(pars_new[1,]), -3)
+pars_init$central$Tehran$start_date <- lubridate::as_date(pars_init$central$Tehran$start_date)
+
+# --------------------------
+## EA worst
+# --------------------------
+
+res <- fetch_res(grp_worst, 1)
+pars <- res$pmcmc_results$results[which.max(res$pmcmc_results$results$log_posterior),]
+pars$start_date <- squire:::offset_to_start_date(res$pmcmc_results$inputs$data$date[1],start_date = pars$start_date)
+
+plot_pars_deaths(res, pars)
+pars_new <- fix_ll(res, pars)
+plot_pars_deaths(res, pars_new)
+pars_new2 <- fix_ll(res, pars_new, 0.75, 1.5, 6)
+plot_pars_deaths(res, pars_new2)
+pars_new3 <- fix_ll(res, pars_new2, 0.95, 1.1, 6)
+plot_pars_deaths(res, pars_new3)
+
+pars_init$worst$`East Azerbaijan`[seq_len(length(pars)-3)] <- head(as.numeric(pars_new3[1,]), -3)
+pars_init$worst$`East Azerbaijan`$start_date <- lubridate::as_date(pars_init$worst$`East Azerbaijan`$start_date)
+
+# --------------------------
+## Tehran worst
+# --------------------------
+
+res <- fetch_res(grp_worst, 8)
+pars <- res$pmcmc_results$results[which.max(res$pmcmc_results$results$log_posterior),]
+pars$start_date <- squire:::offset_to_start_date(res$pmcmc_results$inputs$data$date[1],start_date = pars$start_date)
+
+plot_pars_deaths(res, pars)
+pars_new <- fix_ll(res, pars)
+plot_pars_deaths(res, pars_new)
+pars_new3 <- fix_ll(res, pars_new2, 0.9, 1.2)
+plot_pars_deaths(res, pars_new3)
+
+pars_init$worst$Tehran[seq_len(length(pars)-3)] <- head(as.numeric(pars_new3[1,]), -3)
+pars_init$worst$Tehran$start_date <- lubridate::as_date(pars_init$worst$Tehran$start_date)
+
+# --------------------------
+## WA worst
+# --------------------------
+
+res <- fetch_res(grp_worst, 2)
+pars <- res$pmcmc_results$results[which.max(res$pmcmc_results$results$log_posterior),]
+pars$start_date <- squire:::offset_to_start_date(res$pmcmc_results$inputs$data$date[1],start_date = pars$start_date)
+
+plot_pars_deaths(res, pars)
+pars_new <- fix_ll(res, pars, min = 0.5, 0.5, n_span = 10)
+plot_pars_deaths(res, pars_new)
+
+
+pars_init$worst$`West Azerbaijan`[seq_len(length(pars)-3)] <- head(as.numeric(pars_new[1,]), -3)
+pars_init$worst$`West Azerbaijan`$start_date <- lubridate::as_date(pars_init$worst$`West Azerbaijan`$start_date)
+
+# --------------------------
+## RK worst
+# --------------------------
+
+
+res <- fetch_res(grp_worst, 11)
+pars <- res$pmcmc_results$results[which.max(res$pmcmc_results$results$log_posterior),]
+pars$start_date <- squire:::offset_to_start_date(res$pmcmc_results$inputs$data$date[1],start_date = pars$start_date)
+
+plot_pars_deaths(res, pars)
+pars_new <- fix_ll(res, pars, min = 0.5, 0.5, n_span = 10)
+pars_new2 <- fix_ll(res, pars_new, min = 0.1, 0.1, n_span = 10)
+pars_new3 <- fix_ll(res, pars_new2, min = 0.05, 0.05, n_span = 10)
+
+pars_init$worst$`Razavi Khorasan`[seq_len(length(pars)-3)] <- head(as.numeric(pars_new3[1,]), -3)
+pars_init$worst$`Razavi Khorasan`$start_date <- lubridate::as_date(pars_init$worst$`Razavi Khorasan`$start_date)
--- analysis/cluster_submitting.R
+++ analysis/cluster_submitting.R
@@ -4,16 +4,16 @@
 ## -----------------------------------------------------------------------------
 
 # how long is the mcmc for
-n_mcmc <- 100000
+n_mcmc <- 20000
 
 # should tasks be run in parallel and use multiple chains.
 # leave this as FALSE and see FAQs for more info on this
 model <- "NIMUE"
-dur_R <- 125
-prob_hosp_multiplier <- 1
 replicates <- 10
 rf <- 1
 date <- "2021-10-29"
+mix_mat <- rep(10, 31)
+mix_mat[c(3,11,12,13,23,24,31)] <- 12
 
 # get the states
 provinces <- c(
@@ -26,32 +26,60 @@ provinces <- c(
 )
 
 
-# make the orderly bundles to be run on the cluster
-path_bundles <- cp_path("analysis/orderly_bundles/raw_vaccine_full_data")
-dir.create(path_bundles, showWarnings = FALSE)
+# varying between high and low seroreversion
+# https://www.science.org/doi/10.1126/science.abj9932
+# 50%, 27%, 10%
+# 1/((60*(1/365) - log(0.73))/60)
+dur_R <- c(70, 125, 222)
 
-# bundle these up - this will take like 2 mins to create all the zips.
-bundles <- lapply(
-  provinces, function(x) {
-    orderly::orderly_bundle_pack(
-      path = path_bundles,
-      name = "prov_fit",
-      parameters = list(
-        province = x,
-        replicates=replicates,
-        dur_R=dur_R,
-        model=model,
-        prob_hosp_multiplier=prob_hosp_multiplier,
-        n_mcmc=n_mcmc,
-        date=date
-      )
-    )
-  }
+# increased probability of hospitalisation due to Delta
+# 1·45 [1·08–1·95] https://www.thelancet.com/journals/laninf/article/PIIS1473-3099(21)00475-8/fulltext
+prob_hosp_multiplier <- c(1.95, 1.45, 1.08)
+
+# Hospital capacity impact on IFR.
+# 0.5 = 50% beds are available
+# 1 = 100% beds available
+# 10 will mean no bed limits reached essentially.
+# no limits, multiplier = 10, default = 1, worst = 0.5
+hosp_capacity_multiplier <- c(0.5, 1, 10)
+
+# make the orderly bundles to be run on the cluster
+path_bundles <- c(
+  cp_path("analysis/orderly_bundles/raw_vaccine_complete_worst"),
+  cp_path("analysis/orderly_bundles/raw_vaccine_complete_central"),
+  cp_path("analysis/orderly_bundles/raw_vaccine_complete_best")
 )
 
-# now label these with the states and save the file paths
-names(bundles) <- provinces
-saveRDS(bundles, file.path(path_bundles, "bundles.rds"))
+for(i in 1:3) {
+
+  dir.create(path_bundles[i], showWarnings = FALSE)
+
+  # bundle these up - this will take like 2 mins to create all the zips.
+  bundles <- lapply(
+    seq_along(provinces), function(x) {
+      orderly::orderly_bundle_pack(
+        path = path_bundles[i],
+        name = "prov_fit",
+        parameters = list(
+          province = provinces[x],
+          replicates=replicates,
+          dur_R=dur_R[i],
+          model=model,
+          prob_hosp_multiplier=prob_hosp_multiplier[i],
+          hosp_capacity_multiplier=hosp_capacity_multiplier[i],
+          n_mcmc=n_mcmc,
+          date=date,
+          mix_mat=mix_mat[x]
+        )
+      )
+    }
+  )
+
+  # now label these with the states and save the file paths
+  names(bundles) <- provinces
+  saveRDS(bundles, file.path(path_bundles[i], "bundles.rds"))
+
+}
 
 ## -----------------------------------------------------------------------------
 ## B. CLUSTER SUBMITTING
@@ -116,6 +144,8 @@ obj <- didehpc::queue_didehpc(ctx, config = config)
 ## 3. Submit the jobs
 ## ------------------------------------
 
+submit_jobs <- function(path_bundles) {
+
 workdir <- gsub("raw", "derived", path_bundles)
 dir.create(workdir, recursive = TRUE, showWarnings = FALSE)
 workdir <- normalizePath(workdir)
@@ -129,6 +159,14 @@ split_path <- function(x) if (dirname(x)==x) x else c(basename(x),split_path(dir
 bundle_name <- paste0(tail(rev(split_path(workdir)), 2), collapse = "_")
 grp <- obj$lapply(tasks, orderly::orderly_bundle_run, workdir = workdir,
                   name = bundle_name, overwrite = TRUE)
+
+return(grp)
+
+}
+
+grp_worst <- submit_jobs(cp_path("analysis/orderly_bundles/raw_vaccine_complete_worst"))
+grp_central <- submit_jobs(cp_path("analysis/orderly_bundles/raw_vaccine_complete_central"))
+grp_best <- submit_jobs(cp_path("analysis/orderly_bundles/raw_vaccine_complete_best"))
 
 ## ------------------------------------
 ## 4. Check on our jobs
@@ -160,12 +198,15 @@ obj$submit(grp$ids[to_rerun])
 ## ------------------------------
 
 # use the bundles paths to work out the path to the runs in derived
-grp_grab <- grp
+
+make_fitting_pdf <- function(grp_grab) {
+
 paths <- gsub("raw", "derived", grp_grab$X[grp_grab$status() == "COMPLETE"])
 
 # now extract the fitting.pdf files
-td <- file.path(tempdir(), "pdfs24")
-dir.create(td, showWarnings = FALSE)
+td <- file.path(tempdir(), grp_grab$name, uuid::UUIDgenerate())
+dir.create(td, showWarnings = FALSE, recursive = TRUE)
+
 fits <- lapply(paths, function(x) {
   if(file.exists(x)){
     zip::unzip(
@@ -184,8 +225,14 @@ pdfs <- grep("fitting", list.files(td, full.names = TRUE, recursive = TRUE), val
 # created because it needs to be for orderly to finish the task
 qpdf::pdf_combine(
   input = pdfs[file.size(pdfs) > 0],
-  output = cp_path("analysis/plots/fitting_vacc.pdf")
+  output = cp_path(paste0("analysis/plots/fitting_vacc_", grp_grab$name,".pdf"))
 )
+
+}
+
+make_fitting_pdf(grp_best)
+make_fitting_pdf(grp_central)
+make_fitting_pdf(grp_worst)
 
 ## ------------------------------
 ## 6. Update pars_init to rerun
@@ -199,8 +246,9 @@ qpdf::pdf_combine(
 ## 7. Check individual fits/diagositcs etc
 ## ------------------------------
 
+fetch_res <- function(grp, i) {
 
-res <- readRDS(vapply(gsub("raw", "derived", grp_vacc$X[23]), function(x) {
+res <- readRDS(vapply(gsub("raw", "derived", grp$X[i]), function(x) {
   td <- tempdir()
   zip::unzip(
     zipfile = x,
@@ -212,7 +260,10 @@ res <- readRDS(vapply(gsub("raw", "derived", grp_vacc$X[23]), function(x) {
        value = TRUE)
 }, character(1)))
 
-plot(res$pmcmc_results$results$log_posterior)
+return(res)
+}
+
+plot(res$pmcmc_results$results$log_posterior[seq(1, 50000, 100)])
 
 res$pmcmc_results$chains$chain1$results[1,]
 
